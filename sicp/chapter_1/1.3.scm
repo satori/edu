83ea816f503fb50b1b8ef61e68efbd47bdd3267c
Maxim Bublis <b@codemonkey.ru>
2013-07-14 23:43:05 +0400

    SICP exercise 1.3

---
+++ sicp/chapter_1/1.3.scm
@@ -0,0 +1,12 @@
+(define (square x)
+  (* x x))
+
+(define (max x y)
+  (if (> x y) x y))
+
+(define (min x y)
+  (if (< x y) x y))
+
+(define (sum-of-highest-squares x y z)
+  (+ (square (max x y))
+	 (square (max (min x y) z))))
