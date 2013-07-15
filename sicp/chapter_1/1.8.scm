84509be7d82e08a9b3541f78697d8a2326c16553
Maxim Bublis <b@codemonkey.ru>
2013-07-15 12:15:07 +0400

    SICP exercise 1.8

---
+++ sicp/chapter_1/1.8.scm
@@ -0,0 +1,15 @@
+(define (cbrt x)
+  (define (improve guess)
+	(/ (+ (/ x (square guess))
+		  (* 2 guess))
+	   3))
+
+  (define (good-enough? guess previous-guess)
+	(< (abs (- guess previous-guess)) .001))
+
+  (define (cbrt-iter guess previous-guess)
+	(if (good-enough? guess previous-guess)
+	  guess
+	  (cbrt-iter (improve guess) guess)))
+
+  (cbrt-iter 1.0 0))
