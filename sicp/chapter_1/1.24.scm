a72d09b3ba7ea6c2cd1491ae3dcfa035e4f173f9
Maxim Bublis <b@codemonkey.ru>
2013-07-18 14:52:39 +0400

    Added SICP exercise 1.24.

---
+++ sicp/chapter_1/1.24.scm
@@ -0,0 +1,34 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+		((even? exp)
+		 (remainder (square (expmod base (/ exp 2) m)) m))
+		(else
+		  (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+	(= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+		((fermat-test n) (fast-prime? n (- times 1)))
+		(else false)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (cond ((fast-prime? n 100)
+		 (report-prime (- (runtime) start-time)))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+  (cond ((< start end)
+		 (timed-prime-test start)
+		 (search-for-primes (+ start 1) end))))
