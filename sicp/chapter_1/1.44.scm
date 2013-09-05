550bc591e5e5fbc393e9a7aec9956349be7878cd
Maxim Bublis <b@codemonkey.ru>
2013-09-05 16:29:18 +0400

    Added SICP exercise 1.44

---
+++ sicp/chapter_1/1.44.scm
@@ -0,0 +1,19 @@
+(define (compose f g)
+  (lambda (x)
+	(f (g x))))
+
+(define (repeated f n)
+  (cond ((= n 1) f)
+		(else (compose f (repeated f (- n 1))))))
+
+(define dx 0.00001)
+
+(define (smooth f)
+  (lambda (x)
+	(/ (+ (f (- x dx))
+		  (f x)
+		  (f (+ x dx)))
+	   3)))
+
+(define (n-fold-smooth f n)
+  (repeated smooth n) f)
