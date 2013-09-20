aeb56a38f7da57308d728d25566c3688f039815a
Maxim Bublis <b@codemonkey.ru>
2013-09-20 14:20:28 +0400

    Added SICP exercise 2.43

---
+++ sicp/2.2/2.43.scm
@@ -0,0 +1,49 @@
+(define nil '())
+
+(define (enumerate-interval low high)
+  (if (> low high)
+	nil
+	(cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (fold-right append nil (map proc seq)))
+
+(define empty-board nil)
+
+(define (make-position row col)
+  (cons row col))
+
+(define (position-row p)
+  (car p))
+
+(define (position-col p)
+  (cdr p))
+
+(define (adjoin-position row column rest)
+  (append rest (list (make-position row column))))
+
+(define (safe? col positions)
+  (let ((current (list-ref positions (- col 1)))
+		(other (filter (lambda (p) (not (= (position-col p) col)))
+					   positions)))
+	(= 0 (length (filter (lambda (p)
+						   (or (= (position-row p) (position-row current))
+							   (= (abs (- (position-row p) (position-row current)))
+								  (abs (- (position-col p) (position-col current))))))
+						 other)))))
+
+(define (queens board-size)
+  (define (queen-cols k)
+	(if (= k 0)
+	  (list empty-board)
+	  (filter
+		(lambda (positions) (safe? k positions))
+		(flatmap
+		  (lambda (new-row)
+			(map (lambda (rest-of-queens)
+				   (adjoin-position new-row k rest-of-queens))
+				 (queen-cols (- k 1))))
+		  (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+(queens 8)
