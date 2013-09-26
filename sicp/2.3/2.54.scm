f85d8153ae85be188daee976d61c02db80d4e594
Maxim Bublis <b@codemonkey.ru>
2013-09-26 18:05:00 +0400

    Added SICP exercise 2.54

---
+++ sicp/2.3/2.54.scm
@@ -0,0 +1,8 @@
+(define (equal? a b)
+  (if (and (pair? a) (pair? b))
+	(and (equal? (car a) (car b))
+		 (equal? (cdr a) (cdr b)))
+	(eq? a b)))
+
+(equal? '(this is a list) '(this is a list))
+(equal? '(this is a list) '(this (is a) list))
