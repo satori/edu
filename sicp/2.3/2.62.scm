f941df3ca5296f53aacea99aae92c9c5523afe1c
Maxim Bublis <b@codemonkey.ru>
2013-09-29 22:48:13 +0400

    Added SICP exercise 2.62

---
+++ sicp/2.3/2.62.scm
@@ -0,0 +1,10 @@
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+		((null? set2) set1)
+		((= (car set1) (car set2))
+		 (cons (car set1) (union-set (cdr set1) (cdr set2))))
+		((< (car set1) (car set2))
+		 (cons (car set1) (union-set (cdr set1) set2)))
+		(else (cons (car set2) (union-set set1 (cdr set2))))))
+
+(union-set (list 1 2 3 4 5) (list 4 5 6 7 8 9))
