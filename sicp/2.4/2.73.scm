23a99919a04472190d95734aeaede24ce8d670d3
Maxim Bublis <b@codemonkey.ru>
2013-10-01 14:43:01 +0400

    Added SICP exercise 2.73

---
+++ sicp/2.4/2.73.scm
@@ -0,0 +1,110 @@
+(define *op-table* (make-hash-table))
+
+(define (put op type proc)
+  (hash-table/put! *op-table* (list op type) proc))
+
+(define (get op type)
+  (hash-table/get *op-table* (list op type) '()))
+
+(define (variable? x)
+  (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+	   (variable? v2)
+	   (eq? v1 v2)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+		((variable? exp) (if (same-variable? exp var) 1 0))
+		(else ((get 'deriv (operator exp)) (operands exp)
+										   var))))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+
+(define (install-deriv-package)
+
+  (define (=number? exp num)
+	(and (number? exp)
+		 (= exp num)))
+
+  (define (make-sum a1 a2)
+	(cond ((=number? a1 0) a2)
+		  ((=number? a2 0) a1)
+		  ((and (number? a1)
+				(number? a2))
+		   (+ a1 a2))
+		  (else (list '+ a1 a2))))
+
+  (define (make-product m1 m2)
+	(cond ((or (=number? m1 0)
+			   (=number? m2 0))
+		   0)
+		  ((=number? m1 1) m2)
+		  ((=number? m2 1) m1)
+		  ((and (number? m1)
+				(number? m2))
+		   (* m1 m2))
+		  (else (list '* m1 m2))))
+
+  (define (make-exponentation b e)
+	(cond ((=number? e 0) 1)
+		  ((=number? e 1) b)
+		  ((and (number? b)
+				(number? e))
+		   (expt b e))
+		  (else (list '** b e))))
+
+  (define (addend s)
+	(car s))
+
+  (define (augend s)
+	(cadr s))
+
+  (define (deriv-sum exp var)
+	(make-sum (deriv (addend exp) var)
+			  (deriv (augend exp) var)))
+
+  (define (multiplier p)
+	(car p))
+
+  (define (multiplicand p)
+	(cadr p))
+
+  (define (deriv-product exp var)
+	(make-sum
+	  (make-product (multiplier exp)
+					(deriv (multiplicand exp) var))
+	  (make-product (deriv (multiplier exp) var)
+					(multiplicand exp))))
+
+  (define (base e)
+	(car e))
+
+  (define (exponent e)
+	(cadr e))
+
+  (define (deriv-exponentation exp var)
+	(make-product (make-product (exponent exp)
+								(make-exponentation (base exp)
+													(make-sum (exponent exp) -1)))
+				  (deriv (base exp) var)))
+
+  (put 'deriv '+ deriv-sum)
+  (put 'deriv '* deriv-product)
+  (put 'deriv '** deriv-exponentation))
+
+(install-deriv-package)
+
+(deriv '(+ x 3) 'x)
+
+(deriv '(* x y) 'x)
+
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+(deriv '(** x 3) 'x)
+
+(deriv '(* 3 (** x 2)) 'x)
