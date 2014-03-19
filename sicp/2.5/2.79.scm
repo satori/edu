ea761ec624e757101e6960c1e1106dd2b0afa6cc
Maxim Bublis <b@codemonkey.ru>
2014-03-19 23:22:12 +0400

    Use code from exercise 2.78 instead of copy/paste

--- sicp/2.5/2.79.scm
+++ sicp/2.5/2.79.scm
@@ -1,4 +1,4 @@
-; Copyright (C) 2013 by Maxim Bublis <b@codemonkey.ru>
+; Copyright (C) 2013-2014 by Maxim Bublis <b@codemonkey.ru>
 ;
 ; Permission is hereby granted, free of charge, to any person obtaining
 ; a copy of this software and associated documentation files (the
@@ -19,81 +19,19 @@
 ; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 ; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(define *op-table* (make-hash-table))
+(load "2.78.scm")
 
-(define (put op type proc)
-  (hash-table/put! *op-table* (list op type) proc))
-
-(define (get op type)
-  (hash-table/get *op-table* (list op type) '()))
-
-(define (attach-tag type-tag contents)
-  (if (number? contents)
-    contents
-    (cons type-tag contents)))
-
-(define (type-tag datum)
-  (cond ((number? datum) 'scheme-number)
-        ((pair? datum) (car datum))
-        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
-
-(define (contents datum)
-  (cond ((number? datum) datum)
-        ((pair? datum) (cdr datum))
-        (else (error "Bad tagged datum -- CONTENTS" datum))))
-
-(define (add x y)
-  (apply-generic 'add x y))
-
-(define (sub x y)
-  (apply-generic 'sub x y))
-
-(define (mul x y)
-  (apply-generic 'mul x y))
-
-(define (div x y)
-  (apply-generic 'div x y))
 
 (define (equ? x y)
   (apply-generic 'equ? x y))
 
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags)))
-      (if proc
-        (apply proc (map contents args))
-        (error
-          "No method for these types -- APPLY-GENERIC"
-          (list op type-tags))))))
-
 (define (install-scheme-number-package)
-  (define (tag x)
-    (attach-tag 'scheme-number x))
-
-  (put 'add '(scheme-number scheme-number)
-       (lambda (x y) (tag (+ x y))))
-
-  (put 'sub '(scheme-number scheme-number)
-       (lambda (x y) (tag (- x y))))
-
-  (put 'mul '(scheme-number scheme-number)
-       (lambda (x y) (tag (* x y))))
-
-  (put 'div '(scheme-number scheme-number)
-       (lambda (x y) (tag (/ x y))))
-
-  (put 'make 'scheme-number
-       (lambda (x) (tag x)))
-
   (put 'equ? '(scheme-number scheme-number)
        (lambda (x y) (= x y)))
 
   'done)
 
 (install-scheme-number-package)
-
-(define (make-scheme-number n)
-  ((get 'make 'scheme-number) n))
 
 (define (install-rational-package)
   (define (numer x)
