a0c8d6c259e83ff0691c2e98ed7a2759b296046b
Maxim Bublis <b@codemonkey.ru>
2014-03-31 19:24:03 +0400

    Added SICP exercise 2.89

---
+++ sicp/2.5/2.89.scm
@@ -0,0 +1,55 @@
+; Copyright (C) 2013-2014 by Maxim Bublis <b@codemonkey.ru>
+;
+; Permission is hereby granted, free of charge, to any person obtaining
+; a copy of this software and associated documentation files (the
+; "Software"), to deal in the Software without restriction, including
+; without limitation the rights to use, copy, modify, merge, publish,
+; distribute, sublicense, and/or sell copies of the Software, and to
+; permit persons to whom the Software is furnished to do so, subject to
+; the following conditions:
+;
+; The above copyright notice and this permission notice shall be
+; included in all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+; NONINFRINGMENT. IN NOT EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(load "2.88.scm")
+
+(define (adjoin-term term term-list)
+  (cond ((=zero? (coeff term))
+         term-list)
+        ((= (order term) (length term-list))
+         (cons (coeff term) term-list))
+        (else
+          (adjoin-term term (cons 0 term-list)))))
+
+(define (the-empty-termlist) '())
+
+(define (first-term term-list)
+  (cons
+    (- (length term-list) 1)
+    (car term-list)))
+
+(define (rest-terms term-list)
+  (cdr term-list))
+
+(define (empty-termlist? term-list)
+  (null? term-list))
+
+(define (make-term order coeff)
+  (list order coeff))
+
+(define (order term)
+  (car term))
+
+(define (coeff term)
+  (cadr term))
+
+(adjoin-term (make-term 3 4) '())
+(first-term '(4 0 0 0))
