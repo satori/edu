6edabe1a6f9473f28021414da9c97f5067aa00fd
Maxim Bublis <b@codemonkey.ru>
2014-03-31 17:50:47 +0400

    Added SICP exercise 2.87

---
+++ sicp/2.5/2.87.scm
@@ -0,0 +1,167 @@
+; Copyright (C) 2013-2014 by Maxim Bublis <b@codemonkey.ru>
+;
+; Permission is hereby granted, free of charge, to any person obtaining
+; a copy of this software and associated documentation files (the
+; "Software"), to deal in the Software without restriction, including
+; without limitation the rights to use, copy, modify, merge, publish,
+; distribute, sublicense, and/or sell copies of the Software, and to
+; permit persons to whom the Software is furnished to do so, subject to
+; the following conditions:
+;
+; The above copyright notice and this permission notice shall be
+; included in all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+; NONINFRINGMENT. IN NOT EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(load "2.86.scm")
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+
+  (define (variable p)
+    (car p))
+
+  (define (term-list p)
+    (cdr p))
+
+  (define (variable? x)
+    (symbol? x))
+
+  (define (same-variable? v1 v2)
+    (and (variable? v1)
+         (variable? v2)
+         (eq? v1 v2)))
+
+  ;; representation of terms and term lists
+
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+      term-list
+      (cons term term-list)))
+
+  (define (the-empty-termlist) '())
+
+  (define (first-term term-list)
+    (car term-list))
+
+  (define (rest-terms term-list)
+    (cdr term-list))
+
+  (define (empty-termlist? term-list)
+    (null? term-list))
+
+  (define (make-term order coeff)
+    (list order coeff))
+
+  (define (order term)
+    (car term))
+
+  (define (coeff term)
+    (cadr term))
+
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+            (let ((t1 (first-term L1))
+                  (t2 (first-term L2)))
+              (cond ((> (order t1) (order t2))
+                     (adjoin-term
+                       t1 (add-terms (rest-terms L1) L2)))
+                    ((< (order t1) (order t2))
+                     (adjoin-term
+                       t2 (add-terms L1 (rest-terms L2))))
+                    (else
+                      (adjoin-term
+                        (make-term (order t1)
+                                   (add (coeff t1) (coeff t2)))
+                        (add-terms (rest-terms L1)
+                                   (rest-terms L2)))))))))
+
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+      (the-empty-termlist)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                 (mul-terms (rest-terms L1) L2))))
+
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+      (the-empty-termlist)
+      (let ((t2 (first-term L)))
+        (adjoin-term
+          (make-term (+ (order t1) (order t2))
+                     (mul (coeff t1) (coeff t2)))
+          (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- ADD-POLY"
+             (list p1 p2))))
+
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+             (list p1 p2))))
+
+  (define (zero-poly? p)
+    (let ((terms (term-list p)))
+      (if (empty-termlist? terms)
+        #t
+        (and (=zero? (coeff (first-term terms)))
+             (zero-poly?
+               (make-poly
+                 (variable p)
+                 (rest-terms terms)))))))
+
+  ;; interface to rest of the system
+
+  (define (tag p)
+    (attach-tag 'polynomial p))
+
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+
+  (put '=zero? '(polynomial) zero-poly?)
+
+  'done)
+
+(install-polynomial-package)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
+
+(make-polynomial 'x '((2 5) (1 3) (0 7)))
+(make-polynomial 'x '((2 1) (1 7) (0 -5)))
+
+(make-polynomial 'x (list
+                      (list 3 (make-polynomial 'y '((2 1) (0 1))))
+                      (list 1 (make-polynomial 'y '((1 2))))
+                      (list 0 1)))
+
+(add (make-polynomial 'x '((2 5) (1 3) (0 7)))
+     (make-polynomial 'x '((2 1) (1 7) (0 -5))))
+
+(mul (make-polynomial 'x (list (list 1 (make-polynomial 'i '((1 3) (0 2))))))
+     (make-polynomial 'x (list (list 1 (make-polynomial 'i '((1 3) (0 5)))))))
