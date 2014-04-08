61a8b1c6408c1daee7da35954c48e0ef6f77b06d
Maxim Bublis <b@codemonkey.ru>
2014-04-08 18:32:12 +0400

    codejam: added 2008 Practice Problem C

---
+++ codejam/2008/practice_problems/c.py
@@ -0,0 +1,75 @@
+# Copyright (C) 2014 by Maxim Bublis <b@codemonkey.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OF OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Google Code Jam Practice Problems (2008) Problem C.
+Usage:
+    python c.py < input.txt > output.txt
+"""
+import sys
+
+
+def memoized(func):
+    cache = {}
+    def wrapped(*args):
+        if not args in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapped
+
+
+@memoized
+def solve_floors(drops, breaks):
+    if breaks == 0 or drops == 0:
+        return 0
+
+    return 1 + solve_floors(drops - 1, breaks - 1) + solve_floors(drops - 1, breaks)
+
+
+def solve_drops(floors, breaks):
+    drops = 0
+    while True:
+        if solve_floors(drops, breaks) >= floors:
+            return drops
+        drops += 1
+
+
+def solve_breaks(floors, drops):
+    breaks = 0
+    while True:
+        if solve_floors(drops, breaks) >= floors:
+            return breaks
+        breaks += 1
+
+
+def solve_problem(floors, drops, breaks):
+    solved_floors = -1 if solve_floors(drops, breaks) >= 2**32 else solve_floors(drops, breaks)
+    solved_drops = solve_drops(floors, breaks)
+    solved_breaks = solve_breaks(floors, drops)
+
+    return solved_floors, solved_drops, solved_breaks
+
+
+if __name__ == "__main__":
+    num_of_cases = int(sys.stdin.readline())
+    for i in xrange(1, num_of_cases + 1):
+        floors, drops, breaks = map(int, sys.stdin.readline().split())
+        print "Case #{0}: {1} {2} {3}".format(i, *solve_problem(floors, drops, breaks))
