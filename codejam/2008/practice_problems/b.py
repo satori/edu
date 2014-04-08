8c3e67e275cfffe27588f204ff6c59c3a12735db
Maxim Bublis <b@codemonkey.ru>
2014-04-08 15:18:16 +0400

    codejam: refactored 2008 Practice Problem B

--- codejam/2008/practice_problems/b.py
+++ codejam/2008/practice_problems/b.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 by Maxim Bublis <b@codemonkey.ru>
+# Copyright (C) 2013-2014 by Maxim Bublis <b@codemonkey.ru>
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -38,24 +38,24 @@ WALK_SOUTH = (0, -1)
 WALK_WEST = (-1, 0)
 WALK_EAST = (1, 0)
 
-def walk_maze(maze, path, x, y, direction):
-    cur = (x, y)
+
+def walk_maze(maze, path, position, direction):
+    x, y = position
     for c in path:
         cur = (x, y)
-        room = 0
         if c == 'R':
             direction = (direction[1], -direction[0])
         if c == 'L':
             direction = (-direction[1], direction[0])
         if c == 'W':
             if direction == WALK_NORTH:
-                room |= CAN_WALK_NORTH
+                room = CAN_WALK_NORTH
             elif direction == WALK_SOUTH:
-                room |= CAN_WALK_SOUTH
+                room = CAN_WALK_SOUTH
             elif direction == WALK_WEST:
-                room |= CAN_WALK_WEST
+                room = CAN_WALK_WEST
             elif direction == WALK_EAST:
-                room |= CAN_WALK_EAST
+                room = CAN_WALK_EAST
 
             maze[cur] |= room
 
@@ -68,19 +68,18 @@ def walk_maze(maze, path, x, y, direction):
 def solve_problem(entrance_to_exit, exit_to_entrance):
     maze = collections.defaultdict(lambda: 0)
 
-    x = 0
-    y = 0
+    direction, position = walk_maze(maze, entrance_to_exit[1:], (0, -1), (0, -1))
+    walk_maze(maze, exit_to_entrance[1:], position, (-direction[0], -direction[1]))
 
-    direction, cur = walk_maze(maze, entrance_to_exit[1:], 0, -1, (0, -1))
-    walk_maze(maze, exit_to_entrance[1:], cur[0], cur[1], (-direction[0], -direction[1]))
-
-    keyfunc = lambda room: room[0][1]
+    sort_by = lambda ((x, y), room): (-y, x)
+    group_by = lambda ((x, y), room): y
 
     answer = []
-    for key, group in itertools.groupby(sorted(maze.items(), key=keyfunc, reverse=True), keyfunc):
-        answer.append(''.join(map(lambda room: '{0:x}'.format(room[1]), sorted(group))))
+    for key, group in itertools.groupby(sorted(maze.items(), key=sort_by), group_by):
+        answer.append(''.join(['{0:x}'.format(room[1]) for room in group]))
 
     return '\n'.join(answer)
+
 
 if __name__ == "__main__":
     num_of_cases = int(sys.stdin.readline())
